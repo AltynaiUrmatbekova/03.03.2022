5780e2020040a82cd702d540e9a2d82ad798ddec
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-03-04 12:43:39 +0600

    trigger

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelectorAll('.accordion').forEach(accordion => {
+    const parent = accordion.closest('accordins');
+
+    accordion.querySelector('.accordion__header').addEventListener('click', function() {
+        if (parent && parent.classList.contains('accordions--toggle')) {
+        parent.querySelectorAll('.accordion').forEach(accordion => {
+            accordion.classList.remove("accortion--open");
+        });
+    }
+        accordion.classList.add('accordion--open');
+    });
+});
+
+document.querySelectorAll('([data-modal]').forEach(trigger => {
+    trigger.addEventListener('click', function() {
+       document.getElementById(trigger.dataset.modal).classList.add('modal--open');
+    });
+});
+
+document.querySelectorAll('.modal').forEach(modal => {
+    modal.querySelector('.modal__window').addEventListener('click', function() {
+        modal.classList.remove('modal--open');
+    });
+});
